adc0a32ad1a7b6a32122a8bc1f729c7c6e5b9523
Hong Tran <jessietran@fb.com>
2021-08-03 13:37:35 -0700

    work basically

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      to_do: [],
+      doing: [],
+      done: [],
+      task: ""
+    }
+  }
+
+  addTask(){
+    this.setState({to_do: [...this.state.to_do, { id: this.state.to_do.length + 1, task_name: "" }]})
+  }
+
+  handleChange(e){
+    this.setState({task: e.target.value})
+  }
+
+  handleBlur(){
+    let { to_do } = this.state
+    to_do[to_do.length - 1].task_name = this.state.task
+    console.log('blur', to_do)
+    this.setState({ to_do: to_do })
+  }
+
+  handleDragStart(e, id, cat){
+    e.dataTransfer.setData('preCat', cat)
+    e.dataTransfer.setData('id', id)
+  }
+
+  handleDrop(e, cat){
+    let id = e.dataTransfer.getData('id')
+    let preCat = e.dataTransfer.getData('preCat')
+    let draggedTask = this.state[preCat].filter(task => task.id == id)[0]
+    let preCatList = this.state[preCat]
+    preCatList = preCatList.filter(task => task != draggedTask)
+    this.setState({ 
+      [cat]: [...this.state[cat], draggedTask], 
+      [preCat]: preCatList
+    })
+  
+    
+  }
+
+  handleDragOver(e){
+    e.preventDefault()
+  }
+
+  render(){
+    let { to_do, doing, done } = this.state
+
+    return (
+      <div className="App" style={{ padding: '40px', display: 'flex', justifyContent: 'center'}}>
+        <div className="wrapper" style= {{ display: 'flex', justifyContent: 'space-between', width: '80%'}}>
+          <div className="todo" style={{  width: '30%', position: 'relative'}}>
+            <div style={{ backgroundColor: "#14213d" , color: '#ffffff', border: '1px solid grey', borderRadius: '6px', padding: '6px'}}>Todo</div>
+            <button onClick={() => this.addTask()} style={{ position: 'absolute', left: '-15%', marginTop: '10px'}}>+</button>
+            <div style={{ backgroundColor: "#14213d" , color: '#ffffff', border: '1px solid silver', borderRadius: '6px', minHeight:' 100px', padding: '10px', marginTop: '10px'}}>{to_do.map(task => 
+              <div draggable onDragStart={e => this.handleDragStart(e, task.id, "to_do")} style={{ backgroundColor: "#ffffff", border: '1px solid grey', padding: '5px 0', borderRadius: '4px', }}>
+                <input onChange={(e) => this.handleChange(e)} onBlur={() => this.handleBlur()} style={{ width: '95%', border: 'none', outline: 'none' }} />
+              </div>
+            )}</div>
+          </div>
+          <div  className="doing" style={{  width: '30%'}}>
+            <div style={{ backgroundColor: "#fca311" , border: '1px solid grey', borderRadius: '6px', padding: '6px'}}>Doing</div>
+            <div   onDrop={e => this.handleDrop(e, "doing")} onDragOver={e => this.handleDragOver(e)}  style={{ backgroundColor: "#fca311" , border: '1px solid silver', borderRadius: '6px',  minHeight:' 100px', padding: '10px' , marginTop: '10px'}}>{doing.map(task => 
+              <div  draggable onDragStart={e => this.handleDragStart(e, task.id, "doing")} style={{ border: '1px solid grey', padding: '5px 0', borderRadius: '4px', }}>
+                {task.task_name}
+              </div>
+            )}</div>
+          </div>
+          <div className="done" style={{  width: '30%'}}>
+            <div style={{ backgroundColor: "#e5e5e5" , border: '1px solid grey', borderRadius: '6px', padding: '6px'}}>Done</div>
+            <div onDrop={e => this.handleDrop(e, "done")} onDragOver={e => this.handleDragOver(e)} style={{ backgroundColor: "#e5e5e5" , border: '1px solid silver', borderRadius: '6px', minHeight:' 100px', padding: '10px' , marginTop: '10px'}}>{done.map(task => 
+              <div  draggable onDragStart={e => this.handleDragStart(e, task.id, "done")} style={{ border: '1px solid grey', padding: '5px 0', borderRadius: '4px', }}>
+                {task.task_name}
+              </div>
+            )}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
